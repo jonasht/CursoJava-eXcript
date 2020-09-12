f57f57277cf876f7cdc4e8dc9d1b5d3310210ac1
jonasht <jhenriquet@outlook.com.br>
2020-09-12 06:38:18 -0300

    add

---
+++ 0023-associatividadeDeOperadores/src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("associedade de operadores");
+        int x, y, z;
+        x = y = z = 0;
+
+
+
+        int a = (x + y) + z;
+        System.out.println("a = " + a);
+
+        /*
+        * da direita para a esquerda
+        * 1 ()
+        * 2 *, /, %
+        * 3 +, -
+        * 4 <, <=, >, >=
+        * 5 ==, !=
+        */
+    }
+}
