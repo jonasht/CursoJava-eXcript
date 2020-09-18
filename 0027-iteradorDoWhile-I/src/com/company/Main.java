0dcf1dda56f00797e4f31ee87d9cb0b2ff40d43c
jonasht <jhenriquet@outlook.com.br>
2020-09-17 23:05:54 -0300

    add

---
+++ 0027-iteradorDoWhile-I/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("laço de repetição DO/WHile");
+
+        int i = 0;
+        do {
+            System.out.println("i = " + i);
+            ++i;
+            
+        }while(i <= 10);
+    }
+}
