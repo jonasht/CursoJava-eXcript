b2a2d6e60357c1ddd57feabdbd35b20442adae40
jonasht <jhenriquet@outlook.com.br>
2020-09-19 07:28:53 -0300

    add

---
+++ 0029-lacoDeRepeticaoFor-I/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("\nlooping For I");
+
+        for(int i = 0; i <= 10; ++i){
+            System.out.println(i + "-");
+        }
+
+    }
+}
