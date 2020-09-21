1a3023fb61ac4c5d398410a8f4173d819ca2041e
jonasht <jhenriquet@outlook.com.br>
2020-09-21 13:36:25 -0300

    add

---
+++ 0032-BlocosDeInstrucao-I/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("bloco de instrucao");
+
+        if(1 == 1){
+            System.out.println("true");
+            double d = 1;
+        }
+
+        {
+            System.out.println("the world is blue");
+        }
+
+        
+
+    }
+}
