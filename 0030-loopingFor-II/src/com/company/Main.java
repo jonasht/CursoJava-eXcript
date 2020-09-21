396d8585884c737ae83f9c7362653469331fab78
jonasht <jhenriquet@outlook.com.br>
2020-09-21 13:11:53 -0300

    add

---
+++ 0030-loopingFor-II/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("looping for aula II");
+
+        for (
+                int i = 0; // inicializacao
+                i < 10; // expressao
+                i++ // atualizacao
+        ) {
+            System.out.println("i = " + i);
+        }
+
+
+
+
+
+
+    }
+}
