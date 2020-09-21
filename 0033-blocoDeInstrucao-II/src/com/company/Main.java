40f6881849ee330ea526cca7f3e016ae534b0e44
jonasht <jhenriquet@outlook.com.br>
2020-09-21 13:43:00 -0300

    ad

---
+++ 0033-blocoDeInstrucao-II/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("bloco de instrucao II");
+
+        int i = 0;
+        while (i <= 10);// instrucao nula
+        {
+            i++;
+        }
+    }
+}
