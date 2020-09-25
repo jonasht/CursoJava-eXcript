9c1783740b8fab381e555f2f05579aefee0077f6
jonasht <jhenriquet@outlook.com.br>
2020-09-24 22:41:13 -0300

    update

--- 0037-instrucaoBreak/src/com/company/Main.java
+++ 0037-instrucaoBreak/src/com/company/Main.java
@@ -1,10 +1,17 @@
 package com.company;
 
+import java.util.scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner entrada = new Scanner(System.in);
+        int op = 1;
         System.out.println("instrução break;");
-        
+        while(1){
+            System.out.println("digite 0 para sair ");
+            op = entrada.nextInt();
+        }
     }
 }
